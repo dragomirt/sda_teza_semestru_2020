d2beb8f480e83a00d0f85f99ac7f2b8ccfa9bdf1
dragomirt <dragomirt22@gmail.com>
2020-05-21 10:23:31 +0300

    Adds funcs to header and removes ncurses for better compatibility

--- lib/marfuri.h
+++ lib/marfuri.h
@@ -1,6 +1,27 @@
 #ifndef TEZA_SEMESTRU_SDA_MARFURI_H
 #define TEZA_SEMESTRU_SDA_MARFURI_H
 
-void hello(void);
+struct MARFURI_TEXTILE;
+struct Node;
+typedef struct MARFURI_TEXTILE marfa_t;
+
+//void hello(void);
+struct Node *adaugaLaInceput(struct Node *last, marfa_t data);
+struct Node *adaugaLaSfarsit(struct Node *last, marfa_t data);
+struct Node *adaugaDupa(struct Node *last, marfa_t data, marfa_t item);
+void sterge(struct Node** head, marfa_t data);
+void curataLista(struct Node** head_ref);
+void traversarea(struct Node *last);
+
+void exportToJSON(struct Node *last);
+
+void printRegisteredFiles();
+void creareFisier(const char path[], struct Node *last);
+void citireFisier(const char path[], struct Node **last);
+void readFileFromRegister(struct Node** last, int register_index);
+
+void sort(struct Node **start, int type);
+struct Node *searchByCode(const struct Node *last, const int code);
+struct Node *searchByDenumire(const struct Node *last, const char denumire[]);
 
 #endif //TEZA_SEMESTRU_SDA_MARFURI_H
--- main.c
+++ main.c
@@ -4,28 +4,28 @@
 #include <stdio.h>
 #include "lib/marfuri.h"
 
-#include <ncurses.h>
-#include <unistd.h>
 
-void another_menu(void) {
+int main() {
+    // Meniu
     char input_value;
     int loop_is_running = 1;
 
-    clear();
     while(loop_is_running) {
 
-        printw(""
-               "\n1) blabla"
-               "\nq) Inapoi"
+        printf(""
+               "\n1) Afiseaza Fisiere"
+               "\nq) Iesire din program"
                "\nNavigati: ");
-        refresh();
 
-        scanf("%c", &input_value);
-        clear();
+        scanf("%c%*c", &input_value);
 
         switch (input_value) {
             case '1':
-                printw("helloooo");
+                printRegisteredFiles();
+                break;
+
+            case '2':
+//                hello();
                 break;
 
             case 'q':
@@ -33,52 +33,10 @@ void another_menu(void) {
                 break;
 
             default:
-                printw("huh");
+                printf("huh");
                 break;
         }
     }
-}
-
-int main() {
-    hello();
-    // Meniu
-//    char input_value;
-//    int loop_is_running = 1;
-//
-//    initscr();
-//    noecho();
-//    curs_set(FALSE);
-//
-//    while(loop_is_running) {
-//
-//        printw(""
-//               "\n1) Afiseaza Fisiere"
-//               "\nq) Iesire din program"
-//               "\nNavigati: ");
-//        refresh();
-//
-//        scanf("%c\n", &input_value);
-//        clear();
-//
-//        switch (input_value) {
-//            case '1':
-//                another_menu();
-//                break;
-//
-//            case '2':
-//                hello();
-//                break;
-//
-//            case 'q':
-//                loop_is_running = 0;
-//                break;
-//
-//            default:
-//                printw("huh");
-//                break;
-//        }
-//    }
-//    endwin();
 
     return 0;
 }
